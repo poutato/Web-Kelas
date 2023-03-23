8a5dba13bd56cb11a398f96c0819b08a2c663d6b
Theo <aisyprasetyo81@smp.belajar.id>
2023-03-23 13:35:10 +0700

    menambahkan element pada file hero (revisi)

--- rpl-3/components/Hero.jsx
+++ rpl-3/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({heading,message}) => {
             <div className='p-5 text-white z-[2] mt-[-2rem]'>
                 <h2 className='text-5xl py-2 font-bold'>{heading}</h2>
                 <p className='py-5 text-xl'>{message}</p>
-                <button className='px-8 py-2 border hover:opacity-60'></button>
+                <button className='px-8 py-2 border hover:opacity-60'>amogus</button>
             </div>
         </div>
       </div>
